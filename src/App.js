d587e687372771e0660bbe928c6b40306be26489
sungwonkim <sungwon3708@gmail.com>
2017-10-11 01:13:00 +0900

    four commit : State

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title : "Matrix",
-    poster : "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-  },
-  {
-    title : "Old Boy",
-    poster : "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title : "Star Wars",
-    poster : "http://img.lum.dolimg.com/v1/images/starwars_551c43f4.jpeg"
-  }
-]
-
 class App extends Component {
 
   /*
@@ -32,15 +17,59 @@ class App extends Component {
     componentDidUpdate() 돌고 있던 '로딩중'메세지나 아이콘을 숨기면 된다. 
   */
 
+  /*
+    state 는 리액트 컴포넌틑 안에 있는 오브젝트다.
+    function 처럼 이건 모든 리액트 컴포넌틑 안에 포함되어 있다.
+    state가 바뀔 때마다 컴포넌트는 다시 render 한다. 
+   */
+
+  // state를 만드는 방법
+  /*
+    state.. 는 컴포넌트를 로드하는 방법이고, 디폴트 state 와 함께
+   */
+  state = {
+  }
+
   // 사이클이 시작되었음을 알게되고, 
   componentWillMount() {
     // Movie App을 보면 will mount 일때 api 에 작업을 요청한다.
     console.log('will Mount');
+
   }
 
   // 이제 성공적으로 리액트 세계에 컴포넌트가 자리잡았음을 알게된다.
   componentDidMount() {
     console.log('did Mount');
+    setTimeout(()=>{
+      this.setState({
+        movies: [
+          {
+            title : "Matrix",
+            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+          },
+          {
+            title : "Old Boy",
+            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+          },
+          {
+            title : "Star Wars",
+            poster : "http://img.lum.dolimg.com/v1/images/starwars_551c43f4.jpeg"
+          },
+          {
+            title: "Transpotting",
+            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMzA5Zjc3ZTMtMmU5YS00YTMwLWI4MWUtYTU0YTVmNjVmODZhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+          }
+        ]
+      })
+    }, 5000)
+  }
+
+  //리액트 자체에 기능이 많이 때문에 _ 를 붙임.
+  _renderMovie = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title = { movie.title } poster = { movie.poster } key = { index } />
+    })
+    return movies
   }
 
   // 이제 컴포넌트가 리액트 세계에 '존재'하게 되었을 알게되고,
@@ -48,10 +77,7 @@ class App extends Component {
     console.log('did Render');
     return (
       <div className="App">
-
-        { movies.map((movie, index) => {
-          return <Movie title = { movie.title } poster = { movie.poster } key = { index } />
-        })}
+        {this.state.movies ? this._renderMovie() : 'Loding'}
 
         {/* <Movie title={ movieTitles [0]} poster = { movieImages[0] } />
         <Movie title={ movieTitles [1]} poster = { movieImages[1] } />
